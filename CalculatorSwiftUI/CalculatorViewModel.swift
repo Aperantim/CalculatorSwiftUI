66115177489b8c3ddbeb42bca8d1f3caf0e7cbff
Vitaliy Nesterov <liyuop@mail.ru>
2019-12-14 21:44:42 +0300

    First Commit, almost done

---
+++ CalculatorSwiftUI/CalculatorViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  CalculatorViewModel.swift
+//  CalculatorSwiftUI
+//
+//  Created by Виталий on 14.12.2019.
+//  Copyright © 2019 Aperantim. All rights reserved.
+//
+
+import SwiftUI
+
+class CalculatorViewModel: ObservableObject {
+    
+    @Published var display = CalculatorViewModel.defaultDisplay
+    @Published var pressedOperator = ""
+    
+    static let defaultDisplay = "0"
+    
+    var firstExpression = ""
+    var secondExpression = ""
+    
+    func receiveButtonPress(button: CalculatorButton) {
+        
+        if button.title == "AC" {
+            display = CalculatorViewModel.defaultDisplay
+            firstExpression = ""
+            secondExpression = ""
+            pressedOperator = ""
+            return
+        }
+        
+        if button.title == "+" || button.title == "x" || button.title == "-" || button.title == "+" {
+            pressedOperator = button.title
+            return
+        }
+        
+        if button.title == "=" {
+            let first = Int(firstExpression) ?? 0
+            let second = Int(display) ?? 0
+            
+            if pressedOperator == "+" {
+                display = "\(first + second)"
+            } else if pressedOperator == "-" {
+                display = "\(first - second)"
+            } else if pressedOperator == "x" {
+                display = "\(first * second)"
+            } else if pressedOperator == "/" {
+                display = "\(first / second)"
+            }
+            
+            firstExpression = display
+            secondExpression = ""
+            pressedOperator = ""
+            
+            return
+        }
+        
+        if pressedOperator.isEmpty {
+            
+            if display == CalculatorViewModel.defaultDisplay {
+                display = button.title
+            } else {
+                display += button.title
+            }
+            firstExpression = display
+        } else {
+            
+            if secondExpression.isEmpty {
+                display = button.title
+            } else {
+                display += button.title
+            }
+            secondExpression = display
+        }
+    }
+}
